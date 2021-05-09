835cdbab2ba043bfef2cf7a81ab8a1af3d90bb5a
SynOrUsername <66161049+SynOrUsername@users.noreply.github.com>
2021-05-09 18:46:11 +0200

    Update MainScript.lua

--- CombatRift/MainScript.lua
+++ CombatRift/MainScript.lua
@@ -37,7 +37,7 @@ getgenv().EquipEggs = function(ClientData, Logic, IsInTable, MarketplaceEvents, 
 	end
 end
 
-getgenv().PlaceVersion = 3726
+getgenv().PlaceVersion = 3730
 getgenv().Souls = "\226\128\139PurchaseAllSouls"
 getgenv().Class = "\226\128\139PurchaseClass"
 
